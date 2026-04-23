5ee2e8b9f8f0a9f23deb74bd894629ea3e9d85eb
styf-styf <styf99vn@gmail.com>
2026-04-22 23:17:51 -0500

    dark-0.5

--- frontend/components/Providers.tsx
+++ frontend/components/Providers.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       {children}
